d5feb4fb374e0e33288344fbb426427ddae4b897
Zad Behzadi <behzadi@hotmail.co.uk>
2022-12-14 09:27:25 +0000

    Add files via upload

---
+++ digital content distribution/ContentContractFactory.sol
@@ -0,0 +1,58 @@
+pragma solidity >=0.8.0 <0.9.0;
+
+import "./DappToken.sol";
+import "./SLAContract.sol";
+import "../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract ContentContractFactory is Ownable{
+    address[] public deployedContracts;
+
+    DappToken private _token;
+
+  // With the construction of the factory the digital token contract will be deployed
+    constructor(string memory _name, string memory _symbol, uint8 _decimals)
+        public
+        Ownable()
+    {
+        _token = new DappToken(_name, _symbol, _decimals);
+    }
+
+  /**
+  * @dev Allows owner to create digital content contract for the merchant
+  * @param merchant Address of a Merchant
+  * @param dcHash Hash of the digital content
+  * @param creatorCompensation Compensation of the creator
+  * @param currentPrice Address of a merchant
+  */
+    function deployContentContract(address merchant,
+        string memory dcHash,
+        uint256 creatorCompensation,
+        uint256 currentPrice) public onlyOwner returns (address) {
+
+        SLAContract contentContract = new SLAContract(
+            merchant,
+            owner(),
+            dcHash,
+            creatorCompensation,
+            currentPrice,
+            _token);
+
+        // Add new contract to an array
+        deployedContracts.push(address(contentContract));
+
+        // With the creation of the new contract by the owner give this contract a minter role to mint a tokens
+        _token.addMinter(address(contentContract));
+
+        return address(contentContract);
+    }
+
+    function getContractsByAddress() public view returns (address[] memory) {
+        return deployedContracts;
+    }
+
+
+    function tokenAddress() public view returns (address) {
+        return address(_token);
+    }
+
+}
---
+++ digital content distribution/DappToken.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.8.0 <0.9.0;
+
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+
+contract DappToken is ERC20Mintable, ERC20Detailed {
+    constructor(string memory _name, string memory _symbol, uint8 _decimals)
+        public
+        ERC20Detailed(_name, _symbol, _decimals){}
+}
